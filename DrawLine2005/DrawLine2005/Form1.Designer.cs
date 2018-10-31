bf00a6ecf053af5605242a51fbfc83e76c332c2c
jerhua <jerry.hua@assaabloy.com>
2018-10-31 14:55:27 +0800

    usr subroutine to calculate.

--- DrawLine2005/DrawLine2005/Form1.Designer.cs
+++ DrawLine2005/DrawLine2005/Form1.Designer.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 
 namespace DrawLine2005
 {
-    partial class Form1
+    partial class SVPWM
     {
         /// <summary>
         /// Required designer variable.
@@ -37,42 +37,32 @@ namespace DrawLine2005
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(16, 16);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 31);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Open picture";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(16, 69);
+            this.pictureBox1.Location = new System.Drawing.Point(144, 69);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(720, 400);
+            this.pictureBox1.Size = new System.Drawing.Size(720, 542);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(124, 16);
+            this.button2.Location = new System.Drawing.Point(161, 13);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 31);
+            this.button2.Size = new System.Drawing.Size(122, 48);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Draw line on picture";
+            this.button2.Text = "Draw 5-SVPWM 1 High";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -80,17 +70,40 @@ namespace DrawLine2005
             // 
             this.openFileDialog1.Filter = "Picture file|*.jpg;*.jpeg;*.png;*.bmp";
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Draw 5-SVPWM 1 Low";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(568, 13);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Draw 7-SVPWM";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SVPWM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(777, 513);
+            this.ClientSize = new System.Drawing.Size(1036, 645);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "SVPWM";
+            this.Text = "SVPWM";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -98,12 +111,22 @@ namespace DrawLine2005
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Button button1;
+        private Button button3;
 
         //DATA.
+        int X_LEN = 720; //can be get from user setting.
+        int Y_LEN = 400; //can be get from user setting.
+        int x1 = 0, x2 = 0;
+        int y1 = 0, y2 = 0;
+        float[] DC_U = new float[360];
+        float[] DC_V = new float[360];
+        float[] DC_W = new float[360];
+        float[] DC_COM = new float[360];
+        
     }
 }
 
--- DrawLine2005/DrawLine2005/Form1.cs
+++ DrawLine2005/DrawLine2005/Form1.cs
@@ -9,11 +9,19 @@ using System.Windows.Forms;
 
 namespace DrawLine2005
 {
-    public partial class Form1 : Form
+    public partial class SVPWM : Form
     {
-        public Form1()
+        public SVPWM()
         {
             InitializeComponent();
+        }
+
+        private void SetStartToTargetPoints(int px1, int py1, int px2, int py2)
+        {
+            x1 = px1;
+            y1 = py1;
+            x2 = px2;
+            y2 = py2;
         }
 
         /** 五段式 SVPWM 占空比计算 MATLAB 源代码(一相恒低)   //const high = mr*(1-uvw).
@@ -23,32 +31,8 @@ namespace DrawLine2005
         % 1. calculation of 5 segments SVPWM , one phase is low(connected to
         % GND) during one PWM period
         * */
-        private void button2_Click(object sender, EventArgs e)
+        private void Calculate5SvpwmWithOneLow(float[] u, float[] v, float[] w, float[] com)
         {
-            int X_LEN = 720; //can be get from user setting.
-            int Y_LEN = 400; //can be get from user setting.
-            int x1 = 0, x2 = 0;
-            int y1 = 0, y2 = 0;
-            Bitmap bmp = new Bitmap(X_LEN, Y_LEN);
-            Graphics grp = Graphics.FromImage(bmp);
-            ///Graphics grp = pictureBox1.CreateGraphics(); //can not display correct, and disappear.
-            x1 = 0; y1 = Y_LEN - 0;         //see the right data (0, 0).
-            x2 = 0; y2 = Y_LEN - Y_LEN;     //see the right data (0, Y).
-            grp.DrawLine(new Pen(Color.Gray, 1), new Point(x1, y1), new Point(x2, y2)); //left
-            x1 = X_LEN; y1 = Y_LEN - 0;     //see the right data (X, 0).
-            x2 = X_LEN; y2 = Y_LEN - Y_LEN; //see the right data (X, Y).
-            grp.DrawLine(new Pen(Color.Gray, 2), new Point(x1, y1), new Point(x2, y2)); //right
-            x1 = 0; y1 = Y_LEN - Y_LEN;     //see the right data (0, Y).
-            x2 = X_LEN; y2 = Y_LEN - Y_LEN; //see the right data (X, Y).
-            grp.DrawLine(new Pen(Color.Gray, 1), new Point(x1, y1), new Point(x2, y2)); //top
-            x1 = 0; y1 = Y_LEN - 0;         //see the right data (0, 0).
-            x2 = X_LEN; y2 = Y_LEN - 0;     //see the right data (X, 0).
-            grp.DrawLine(new Pen(Color.Gray, 1), new Point(x1, y1), new Point(x2, y2)); //bottom
-
-            float[] DC_U = new float[360];
-            float[] DC_V = new float[360];
-            float[] DC_W = new float[360];
-
             double mr = (double)Y_LEN;              //modulation radio.
             double sr = (double)(X_LEN / 360);      //scale radio.
 
@@ -58,49 +42,127 @@ namespace DrawLine2005
             for (int i = 0; i < 360; i++)
             {
                 a = ((double)i / 180) * pi;
-                if ((i>=0) && (i<60))
+                if ((i >= 0) && (i < 60))
                 {
-                    DC_U[i] = (float)(mr * Math.Cos(a - pi / 6));
-                    DC_V[i] = (float)(mr * Math.Sin(a));
-                    DC_W[i] = 0;
+                    u[i] = (float)(mr * Math.Cos(a - pi / 6));
+                    v[i] = (float)(mr * Math.Sin(a));
+                    w[i] = 0;
                 }
                 else if ((i >= 60) && (i < 120))
                 {
-                    DC_U[i] = (float)(mr * Math.Cos(a - pi / 6));
-                    DC_V[i] = (float)(mr * Math.Sin(a));
-                    DC_W[i] = 0;
+                    u[i] = (float)(mr * Math.Cos(a - pi / 6));
+                    v[i] = (float)(mr * Math.Sin(a));
+                    w[i] = 0;
                 }
                 else if ((i >= 120) && (i < 180))
                 {
-                    DC_U[i] = 0;
-                    DC_V[i] = (float)(mr * Math.Sin(a - pi / 3));
-                    DC_W[i] = (float)(-mr * Math.Cos(a - pi / 6));
+                    u[i] = 0;
+                    v[i] = (float)(mr * Math.Sin(a - pi / 3));
+                    w[i] = (float)(-mr * Math.Cos(a - pi / 6));
                 }
                 else if ((i >= 120) && (i < 180))
                 {
-                    DC_U[i] = 0;
-                    DC_V[i] = (float)(mr * Math.Sin(a - pi / 3));
-                    DC_W[i] = (float)(-mr * Math.Cos(a - pi / 6));
+                    u[i] = 0;
+                    v[i] = (float)(mr * Math.Sin(a - pi / 3));
+                    w[i] = (float)(-mr * Math.Cos(a - pi / 6));
                 }
                 else if ((i >= 180) && (i < 240))
                 {
-                    DC_U[i] = 0;
-                    DC_V[i] = (float)(mr * Math.Sin(a - pi / 3));
-                    DC_W[i] = (float)(-mr * Math.Cos(a - pi / 6));
+                    u[i] = 0;
+                    v[i] = (float)(mr * Math.Sin(a - pi / 3));
+                    w[i] = (float)(-mr * Math.Cos(a - pi / 6));
                 }
                 else if ((i >= 240) && (i < 300))
                 {
-                    DC_U[i] = (float)(mr * Math.Cos(a + pi / 6));
-                    DC_V[i] = 0;
-                    DC_W[i] = (float)(-mr * Math.Sin(a));
+                    u[i] = (float)(mr * Math.Cos(a + pi / 6));
+                    v[i] = 0;
+                    w[i] = (float)(-mr * Math.Sin(a));
                 }
                 else if ((i >= 300) && (i < 360))
                 {
-                    DC_U[i] = (float)(mr * Math.Cos(a + pi / 6));
-                    DC_V[i] = 0;
-                    DC_W[i] = (float)(-mr * Math.Sin(a));
+                    u[i] = (float)(mr * Math.Cos(a + pi / 6));
+                    v[i] = 0;
+                    w[i] = (float)(-mr * Math.Sin(a));
                 }
+                com[i] = (u[i] + v[i] + w[i]) / 3;
             }//for calculate.
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(X_LEN, Y_LEN);
+            Graphics grp = Graphics.FromImage(bmp);
+            ///Graphics grp = pictureBox1.CreateGraphics(); //can not display correct, and disappear.
+            SetStartToTargetPoints(0, Y_LEN - 0,
+                                   0, Y_LEN - Y_LEN); //see the right data (0, 0)->(0, Y).
+            grp.DrawLine(new Pen(Color.Gray, 1), new Point(x1, y1), new Point(x2, y2)); //left
+
+            SetStartToTargetPoints(X_LEN, Y_LEN - 0,
+                                   X_LEN, Y_LEN - Y_LEN); //see the right data (X, 0)->(X, Y).
+            grp.DrawLine(new Pen(Color.Gray, 2), new Point(x1, y1), new Point(x2, y2)); //right
+
+            SetStartToTargetPoints(0, Y_LEN - Y_LEN,
+                                   X_LEN, Y_LEN - Y_LEN); //see the right data (0, Y)->(X, Y).
+            grp.DrawLine(new Pen(Color.Gray, 1), new Point(x1, y1), new Point(x2, y2)); //top
+
+            SetStartToTargetPoints(0, Y_LEN - 0,
+                                   X_LEN, Y_LEN - 0); //see the right data (0, 0)->(X,0).
+            grp.DrawLine(new Pen(Color.Gray, 1), new Point(x1, y1), new Point(x2, y2)); //bottom
+
+            Calculate5SvpwmWithOneLow(DC_U, DC_V, DC_W, DC_COM);
+            //double mr = (double)Y_LEN;              //modulation radio.
+            double sr = (double)(X_LEN / 360);      //scale radio.
+
+            //double pi = Math.PI;
+            //double a = 0;                           //alpha, or theta
+
+            //for (int i = 0; i < 360; i++)
+            //{
+            //    a = ((double)i / 180) * pi;
+            //    if ((i>=0) && (i<60))
+            //    {
+            //        DC_U[i] = (float)(mr * Math.Cos(a - pi / 6));
+            //        DC_V[i] = (float)(mr * Math.Sin(a));
+            //        DC_W[i] = 0;
+            //    }
+            //    else if ((i >= 60) && (i < 120))
+            //    {
+            //        DC_U[i] = (float)(mr * Math.Cos(a - pi / 6));
+            //        DC_V[i] = (float)(mr * Math.Sin(a));
+            //        DC_W[i] = 0;
+            //    }
+            //    else if ((i >= 120) && (i < 180))
+            //    {
+            //        DC_U[i] = 0;
+            //        DC_V[i] = (float)(mr * Math.Sin(a - pi / 3));
+            //        DC_W[i] = (float)(-mr * Math.Cos(a - pi / 6));
+            //    }
+            //    else if ((i >= 120) && (i < 180))
+            //    {
+            //        DC_U[i] = 0;
+            //        DC_V[i] = (float)(mr * Math.Sin(a - pi / 3));
+            //        DC_W[i] = (float)(-mr * Math.Cos(a - pi / 6));
+            //    }
+            //    else if ((i >= 180) && (i < 240))
+            //    {
+            //        DC_U[i] = 0;
+            //        DC_V[i] = (float)(mr * Math.Sin(a - pi / 3));
+            //        DC_W[i] = (float)(-mr * Math.Cos(a - pi / 6));
+            //    }
+            //    else if ((i >= 240) && (i < 300))
+            //    {
+            //        DC_U[i] = (float)(mr * Math.Cos(a + pi / 6));
+            //        DC_V[i] = 0;
+            //        DC_W[i] = (float)(-mr * Math.Sin(a));
+            //    }
+            //    else if ((i >= 300) && (i < 360))
+            //    {
+            //        DC_U[i] = (float)(mr * Math.Cos(a + pi / 6));
+            //        DC_V[i] = 0;
+            //        DC_W[i] = (float)(-mr * Math.Sin(a));
+            //    }
+            //    DC_COM[i] = (DC_U[i] + DC_V[i] + DC_W[i])/3;
+            //}//for calculate.
             //draw lines.
             for (int j = 0; j < X_LEN; j++)
             {
@@ -109,25 +171,15 @@ namespace DrawLine2005
                 {
                     k = 360-2;
                 }
-                x1 = j; y1 = Y_LEN - 0;
-                x2 = j+1; y2 = Y_LEN - 0;
-                grp.DrawLine(new Pen(Color.Gray, 1), new Point(x1, y1), new Point(x2, y2)); //bottom
                 grp.DrawLine(new Pen(Color.Red, 2), new Point(j, Y_LEN - (int)DC_U[k]), new Point(j + 1, Y_LEN - (int)DC_U[k + 1]));
                 grp.DrawLine(new Pen(Color.Blue, 2), new Point(j, Y_LEN - (int)DC_V[k]), new Point(j + 1, Y_LEN - (int)DC_V[k + 1]));
                 grp.DrawLine(new Pen(Color.Green, 2), new Point(j, Y_LEN - (int)DC_W[k]), new Point(j + 1, Y_LEN - (int)DC_W[k + 1]));
+                grp.DrawLine(new Pen(Color.LightPink, 2), new Point(j, Y_LEN - (int)DC_COM[k]), new Point(j + 1, Y_LEN - (int)DC_COM[k + 1]));
             }
 
             //grp.DrawLine(new Pen(Color.Red, 10), new Point(0, 0), new Point(100, 100));
             //grp.DrawLine(new Pen(Color.Red, 10), new Point(0, Y_LEN - 0), new Point(100, Y_LEN - 100));//Y should move to left bottom point.
             pictureBox1.Image = bmp;
         }//button 2 click.
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
-            }
-        }
     }
 }
