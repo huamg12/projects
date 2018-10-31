fed0652981def8e47747536eac82460c8227c35a
jerhua <jerry.hua@assaabloy.com>
2018-10-31 22:17:16 +0800

    7 segment SVPWM ok.

--- DrawLine2005/DrawLine2005/Form1.Designer.cs
+++ DrawLine2005/DrawLine2005/Form1.Designer.cs
@@ -50,7 +50,7 @@ namespace DrawLine2005
             this.pictureBox1.Location = new System.Drawing.Point(16, 69);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(956, 542);
+            this.pictureBox1.Size = new System.Drawing.Size(723, 446);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -60,9 +60,9 @@ namespace DrawLine2005
             this.button2.Location = new System.Drawing.Point(161, 13);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(122, 48);
+            this.button2.Size = new System.Drawing.Size(125, 48);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Draw 5-SVPWM 1 High";
+            this.button2.Text = "Draw 5-SVPWM    1 High";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -75,28 +75,28 @@ namespace DrawLine2005
             this.button1.Location = new System.Drawing.Point(16, 13);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 48);
+            this.button1.Size = new System.Drawing.Size(137, 48);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Draw 5-SVPWM 1 Low";
+            this.button1.Text = "Draw 5-SVPWM    1 Low";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button2_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(568, 13);
+            this.button3.Location = new System.Drawing.Point(364, 13);
             this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(122, 48);
             this.button3.TabIndex = 2;
             this.button3.Text = "Draw 7-SVPWM";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button2_Click);
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // SVPWM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1036, 645);
+            this.ClientSize = new System.Drawing.Size(780, 528);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
@@ -126,7 +126,12 @@ namespace DrawLine2005
         float[] DC_V = new float[X_LEN];
         float[] DC_W = new float[X_LEN];
         float[] DC_COM = new float[X_LEN];
-        
+        double Ub = (double)Y_LEN;              //Voltage base.
+        double mr = 1;                          //modulation radio.
+        double sr = (double)(360) / X_LEN;      //scale radio.       x/X_LEN = i/360.   i = x * (360/X_LEN)
+        double a = 0;                           //alpha, or theta
+        double i = 0;                           //angle index.
+        double pi = 0;
     }
 }
 
--- DrawLine2005/DrawLine2005/Form1.cs
+++ DrawLine2005/DrawLine2005/Form1.cs
@@ -14,100 +14,13 @@ namespace DrawLine2005
         public SVPWM()
         {
             InitializeComponent();
+            pi = Math.PI;
         }
 
-        private void SetStartToTargetPoints(int px1, int py1, int px2, int py2)
+        private void DrawPictureFrameLines(Graphics grp)
         {
-            x1 = px1;
-            y1 = py1;
-            x2 = px2;
-            y2 = py2;
-        }
-
-        /** 五段式 SVPWM 占空比计算 MATLAB 源代码(一相恒低)   //const high = mr*(1-uvw).
-        % File Name: DutyCycle_5_Segment_SVPWM_OnePhaseToGND.m
-        % Author: Jerry.Hua
-        % Description:
-        % 1. calculation of 5 segments SVPWM , one phase is low(connected to
-        % GND) during one PWM period
-        * */
-        private void Calculate5SvpwmWithOneLow(float[] u, float[] v, float[] w, float[] com)
-        {
-            double mr = (double)Y_LEN;              //modulation radio.
-            double sr = (double)(X_LEN / 360);      //scale radio.
-
-            double pi = Math.PI;
-            double a = 0;                           //alpha, or theta
-
-            int i = 0;                              //angle index.
-            int x = 0;                              //add index.
-            int range = X_LEN;
-
-            if (range > 720)
-            {
-                range = 720; //max to display 2 circles.
-            }
-            if (range < 360)
-            {
-                range = 360; //min to display 1 circles.
-            }
-            for (x = 0; x < range; x++)
-            {
-                i = x % 360;
-                a = ((double)i / 180) * pi;
-                if ((i >= 0) && (i < 60))
-                {
-                    u[x] = (float)(mr * Math.Cos(a - pi / 6));
-                    v[x] = (float)(mr * Math.Sin(a));
-                    w[x] = 0;
-                }
-                else if ((i >= 60) && (i < 120))
-                {
-                    u[x] = (float)(mr * Math.Cos(a - pi / 6));
-                    v[x] = (float)(mr * Math.Sin(a));
-                    w[x] = 0;
-                }
-                else if ((i >= 120) && (i < 180))
-                {
-                    u[x] = 0;
-                    v[x] = (float)(mr * Math.Sin(a - pi / 3));
-                    w[x] = (float)(-mr * Math.Cos(a - pi / 6));
-                }
-                else if ((i >= 120) && (i < 180))
-                {
-                    u[x] = 0;
-                    v[x] = (float)(mr * Math.Sin(a - pi / 3));
-                    w[x] = (float)(-mr * Math.Cos(a - pi / 6));
-                }
-                else if ((i >= 180) && (i < 240))
-                {
-                    u[x] = 0;
-                    v[x] = (float)(mr * Math.Sin(a - pi / 3));
-                    w[x] = (float)(-mr * Math.Cos(a - pi / 6));
-                }
-                else if ((i >= 240) && (i < 300))
-                {
-                    u[x] = (float)(mr * Math.Cos(a + pi / 6));
-                    v[x] = 0;
-                    w[x] = (float)(-mr * Math.Sin(a));
-                }
-                else if ((i >= 300) && (i < 360))
-                {
-                    u[x] = (float)(mr * Math.Cos(a + pi / 6));
-                    v[x] = 0;
-                    w[x] = (float)(-mr * Math.Sin(a));
-                }
-                com[x] = (u[x] + v[x] + w[x]) / 3;
-            }//for calculate.
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Bitmap bmp = new Bitmap(X_LEN, Y_LEN);
-            Graphics grp = Graphics.FromImage(bmp);
-            ///Graphics grp = pictureBox1.CreateGraphics(); //can not display correct, and disappear.
             SetStartToTargetPoints(0, Y_LEN - 0,
-                                   0, Y_LEN - Y_LEN); //see the right data (0, 0)->(0, Y).
+                           0, Y_LEN - Y_LEN); //see the right data (0, 0)->(0, Y).
             grp.DrawLine(new Pen(Color.Gray, 1), new Point(x1, y1), new Point(x2, y2)); //left
 
             SetStartToTargetPoints(X_LEN, Y_LEN - 0,
@@ -121,15 +34,14 @@ namespace DrawLine2005
             SetStartToTargetPoints(0, Y_LEN - 0,
                                    X_LEN, Y_LEN - 0); //see the right data (0, 0)->(X,0).
             grp.DrawLine(new Pen(Color.Gray, 1), new Point(x1, y1), new Point(x2, y2)); //bottom
+        }
 
-            Calculate5SvpwmWithOneLow(DC_U, DC_V, DC_W, DC_COM);
-            //double mr = (double)Y_LEN;              //modulation radio.
-            //double sr = (double)(X_LEN / 360);      //scale radio.
-
-            //draw lines.
+        private void DrawPictureDataLines(Graphics grp)
+        {
             for (int x = 0; x < X_LEN; x++)
             {
-                //int k = (int)((double)x / sr);
+                //double sr = (double)(X_LEN / 360);      //scale radio.
+                //int k = (int)((double)x / sr);          //just usr 360 points.
                 int k = x;
                 if (k > X_LEN - 2)
                 {
@@ -140,10 +52,253 @@ namespace DrawLine2005
                 grp.DrawLine(new Pen(Color.Green, 2), new Point(x, Y_LEN - (int)DC_W[k]), new Point(x + 1, Y_LEN - (int)DC_W[k + 1]));
                 grp.DrawLine(new Pen(Color.LightPink, 2), new Point(x, Y_LEN - (int)DC_COM[k]), new Point(x + 1, Y_LEN - (int)DC_COM[k + 1]));
             }
+        }
 
-            //grp.DrawLine(new Pen(Color.Red, 10), new Point(0, 0), new Point(100, 100));
-            //grp.DrawLine(new Pen(Color.Red, 10), new Point(0, Y_LEN - 0), new Point(100, Y_LEN - 100));//Y should move to left bottom point.
+        private void SetStartToTargetPoints(int px1, int py1, int px2, int py2)
+        {
+            x1 = px1;
+            y1 = py1;
+            x2 = px2;
+            y2 = py2;
+        }
+
+        private int GetDrawLengthRange(int LenRange)
+        {
+            int LimitRange = LenRange;
+            if (LenRange > 720)
+            {
+                LimitRange = 720; //max to display 2 circles.
+            }
+            else if (LenRange < 360)
+            {
+                LimitRange = 360; //min to display 1 circles.
+            }
+            else
+            {
+                LimitRange = LenRange;
+            }
+            return LimitRange;
+        }
+
+         private void ClearCalculatedData()
+        {
+            for (int i = 0; i < X_LEN; i++)
+            {
+                DC_U[i] = 0;
+                DC_V[i] = 0;
+                DC_W[i] = 0;
+                DC_COM[i] = 0;
+            }
+        }
+
+        /** 五段式 SVPWM 占空比计算 MATLAB 源代码(一相恒低)   //const high = mr*(1-uvw).
+         *  File Name: DutyCycle_5_Segment_SVPWM_OnePhaseToGND.m
+         *  Author: Jerry.Hua
+         *  Description:
+         *  1. calculation of 5 segments SVPWM , one phase is low(connected to
+         *  GND) during one PWM period
+         * */
+        private void Calculate5SvpwmWithOneLow(float[] u, float[] v, float[] w, float[] com)
+        {
+            int range = GetDrawLengthRange( X_LEN );
+            for (int x = 0; x < range; x++)
+            {
+                i = (double)x * sr;
+                a = (i / 180) * pi;
+                if ((i >= 0) && (i < 60))
+                {
+                    u[x] = (float)(Ub * mr * Math.Cos(a - pi / 6));
+                    v[x] = (float)(Ub * mr * Math.Sin(a));
+                    w[x] = 0;
+                }
+                else if ((i >= 60) && (i < 120))
+                {
+                    u[x] = (float)(Ub * mr * Math.Cos(a - pi / 6));
+                    v[x] = (float)(Ub * mr * Math.Sin(a));
+                    w[x] = 0;
+                }
+                else if ((i >= 120) && (i < 180))
+                {
+                    u[x] = 0;
+                    v[x] = (float)(Ub * mr * Math.Sin(a - pi / 3));
+                    w[x] = (float)(-Ub * mr * Math.Cos(a - pi / 6));
+                }
+                else if ((i >= 180) && (i < 240))
+                {
+                    u[x] = 0;
+                    v[x] = (float)(Ub * mr * Math.Sin(a - pi / 3));
+                    w[x] = (float)(-Ub * mr * Math.Cos(a - pi / 6));
+                }
+                else if ((i >= 240) && (i < 300))
+                {
+                    u[x] = (float)(Ub * mr * Math.Cos(a + pi / 6));
+                    v[x] = 0;
+                    w[x] = (float)(-Ub * mr * Math.Sin(a));
+                }
+                else if ((i >= 300) && (i < 360))
+                {
+                    u[x] = (float)(Ub * mr * Math.Cos(a + pi / 6));
+                    v[x] = 0;
+                    w[x] = (float)(-Ub * mr * Math.Sin(a));
+                }
+                com[x] = (u[x] + v[x] + w[x]) / 3;
+            }//for calculate.
+        }
+
+        /** 五段式 SVPWM 占空比计算 MATLAB 源代码(一相恒高)   //const high = mr*(1-uvw).
+         *  File Name: DutyCycle_5_Segment_SVPWM_OnePhaseToGND.m
+         *  Author: Jerry.Hua
+         *  Description:
+         *  1. calculation of 5 segments SVPWM , one phase is low(connected to
+         *  GND) during one PWM period
+         * */
+        private void Calculate5SvpwmWithOneHigh(float[] u, float[] v, float[] w, float[] com)
+        {
+            int range = GetDrawLengthRange(X_LEN);
+            for (int x = 0; x < range; x++)
+            {
+                i = (double)x * sr;
+                a = (i / 180) * pi;
+                if ((i >= 0) && (i < 60))
+                {
+                    u[x] = (float)(Ub * mr * Math.Cos(a - pi / 6));
+                    v[x] = (float)(Ub * mr * Math.Sin(a));
+                    w[x] = 0;
+                }
+                else if ((i >= 60) && (i < 120))
+                {
+                    u[x] = (float)(Ub * mr * Math.Cos(a - pi / 6));
+                    v[x] = (float)(Ub * mr * Math.Sin(a));
+                    w[x] = 0;
+                }
+                else if ((i >= 120) && (i < 180))
+                {
+                    u[x] = 0;
+                    v[x] = (float)(Ub * mr * Math.Sin(a - pi / 3));
+                    w[x] = (float)(-Ub * mr * Math.Cos(a - pi / 6));
+                }
+                else if ((i >= 180) && (i < 240))
+                {
+                    u[x] = 0;
+                    v[x] = (float)(Ub * mr * Math.Sin(a - pi / 3));
+                    w[x] = (float)(-Ub * mr * Math.Cos(a - pi / 6));
+                }
+                else if ((i >= 240) && (i < 300))
+                {
+                    u[x] = (float)(Ub * mr * Math.Cos(a + pi / 6));
+                    v[x] = 0;
+                    w[x] = (float)(-Ub * mr * Math.Sin(a));
+                }
+                else if ((i >= 300) && (i < 360))
+                {
+                    u[x] = (float)(Ub * mr * Math.Cos(a + pi / 6));
+                    v[x] = 0;
+                    w[x] = (float)(-Ub * mr * Math.Sin(a));
+                }
+                u[x] = (float)(Ub * mr) - u[x];
+                v[x] = (float)(Ub * mr) - v[x];
+                w[x] = (float)(Ub * mr) - w[x];
+                com[x] = (u[x] + v[x] + w[x]) / 3;
+            }//for calculate.
+        }
+
+        /** 七段式 SVPWM 占空比计算 MATLAB 源代码
+         *  File Name: DutyCycle_5_Segment_SVPWM_OnePhaseToGND.m
+         *  Author: Jerry.Hua
+         *  Description:
+         *  1. calculation of 7 segments SVPWM.
+         * */
+        private void Calculate7Svpwm(float[] u, float[] v, float[] w, float[] com)
+        {
+            int range = GetDrawLengthRange(X_LEN);
+            for (int x = 0; x < range; x++)
+            {
+                //i = x % 360;                  //display in circles.
+                //i = x * 360 / range;          //scale display.
+                i = (double)x * sr;             //scale display.
+                a = (i / 180) * pi;
+                if ((i >= 0) && (i < 60))
+                {
+                    u[x] = (float)(Ub * (0.5 * (1 + mr * Math.Cos(a - pi / 6))));
+                    v[x] = (float)(Ub * (0.5 * (1 - mr * Math.Cos(a - pi / 6)) + mr * Math.Sin(a)));
+                    w[x] = (float)(Ub * (0.5 * (1 - mr * Math.Cos(a - pi / 6))));
+                }
+                else if ((i >= 60) && (i < 120))
+                {
+                    u[x] = (float)(Ub * (0.5 * (1 - mr * Math.Sin(a)) + mr * Math.Sin(a + pi / 3)));
+                    v[x] = (float)(Ub * (0.5 * (1 + mr * Math.Sin(a))));
+                    w[x] = (float)(Ub * (0.5 * (1 - mr * Math.Sin(a))));
+                }
+                else if ((i >= 120) && (i < 180))
+                {
+                    u[x] = (float)(Ub * 0.5 * (1 - mr * Math.Sin(a - pi / 3)));
+                    v[x] = (float)(Ub * 0.5 * (1 + mr * Math.Sin(a - pi / 3)));
+                    w[x] = (float)(Ub * (0.5 * (1 - mr * Math.Sin(a - pi / 3)) - mr * Math.Sin(a + pi / 3)));
+                }
+                else if ((i >= 180) && (i < 240))
+                {
+                    u[x] = (float)(Ub * 0.5 * (1 + mr * Math.Cos(a - pi / 6)));
+                    v[x] = (float)(Ub * (0.5 * (1 + mr * Math.Cos(a - pi / 6)) + mr * Math.Sin(a - pi / 3)));
+                    w[x] = (float)(Ub * 0.5 * (1 - mr * Math.Cos(a - pi / 6)));
+                }
+                else if ((i >= 240) && (i < 300))
+                {
+                    u[x] = (float)(Ub * (0.5 * (1 + mr * Math.Sin(a)) - mr * Math.Sin(a - pi / 3)));
+                    v[x] = (float)(Ub * 0.5 * (1 + mr * Math.Sin(a)));
+                    w[x] = (float)(Ub * 0.5 * (1 - mr * Math.Sin(a)));
+                }
+                else if ((i >= 300) && (i < 360))
+                {
+                    u[x] = (float)(Ub * 0.5 * (1 + mr * Math.Cos(a + pi / 6)));
+                    v[x] = (float)(Ub * 0.5 * (1 - mr * Math.Cos(a + pi / 6)));
+                    w[x] = (float)(Ub * (0.5 * (1 - mr * Math.Cos(a + pi / 6)) - mr * Math.Sin(a)));
+                }
+                //com[x] = (u[x] + v[x] + w[x]) / 3;
+            }//for calculate.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(X_LEN, Y_LEN);
+            Graphics grp = Graphics.FromImage(bmp);
+            DrawPictureFrameLines(grp);
+            //calculate
+            ClearCalculatedData();
+            Calculate5SvpwmWithOneLow(DC_U, DC_V, DC_W, DC_COM);
+            //draw lines.
+            DrawPictureDataLines(grp);
             pictureBox1.Image = bmp;
-        }//button 2 click.
+        }//button 1 click.
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(X_LEN, Y_LEN);
+            Graphics grp = Graphics.FromImage(bmp);
+            DrawPictureFrameLines(grp);
+            //calculate
+            ClearCalculatedData();
+            Calculate5SvpwmWithOneHigh(DC_U, DC_V, DC_W, DC_COM);
+            //draw lines.
+            DrawPictureDataLines(grp);
+            pictureBox1.Image = bmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(X_LEN, Y_LEN);
+            Graphics grp = Graphics.FromImage(bmp);
+            DrawPictureFrameLines(grp);
+            //calculate
+            ClearCalculatedData();
+            Calculate7Svpwm(DC_U, DC_V, DC_W, DC_COM);
+            //draw lines.
+            DrawPictureDataLines(grp);
+            pictureBox1.Image = bmp;
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            ClearCalculatedData();
+        }
     }
 }
