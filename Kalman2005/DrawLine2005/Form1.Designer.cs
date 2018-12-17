d82d55d4b32ef5fe1e57c44b214175e8e2a85f0e
jerhua <jerry.hua@assaabloy.com>
2018-12-17 20:00:50 +0800

    kalman filter.

---
+++ Kalman2005/DrawLine2005/Form1.Designer.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// 一维滤波器信息结构体
+class kalmanType
+{
+    public double X;   // k-1时刻的滤波值，即是k-1时刻的值
+    public double P;   // 估计误差协方差
+    public double K;   // Kalamn增益
+    public double A;   // x(n)=A*x(n-1)+u(n), u(n)~N(0,Q)
+    public double H;   // z(n)=H*x(n)+w(n),   w(n)~N(0,R)
+    public double Q;   // 过程噪声偏差的方差
+    public double R;   // 测量噪声偏差，(系统搭建好以后，通过测量统计实验获得)
+};
+
+namespace Kalman2005
+{
+
+    partial class KalmanFilter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 62);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(954, 411);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Picture file|*.jpg;*.jpeg;*.png;*.bmp";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mod Radio(0~1):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(148, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0.9";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 502);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "KalmanFilter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // KalmanFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(1018, 572);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(720, 360);
+            this.Name = "KalmanFilter";
+            this.Text = "SVPWM";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+        //static DATA.
+        static int CIRCLE = 360;
+        static int BUF_LEN = 720;
+        double[] DC_U = new double[BUF_LEN];        //Duty Cycle of U phase.
+        double[] DC_V = new double[BUF_LEN];        //Duty Cycle of V phase.
+        double[] DC_W = new double[BUF_LEN];        //Duty Cycle of W phase.
+        double[] DC_COM = new double[BUF_LEN];      //Duty Cycle of U+V+W phase.
+        //variable 
+        int X_LEN = 0;
+        int Y_LEN = 0;
+        double Ub = 0;
+        double xtoa = 0;
+        double mr = 0;                          //modulation radio.
+        //double pi = 0;
+        //object
+        Bitmap bmp;
+        Graphics grp;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        //private Label xLabel[12];
+
+        kalmanType kal = new kalmanType();
+        Random rQ = new Random();
+        Random rR = new Random();
+    }
+}
+
---
+++ Kalman2005/DrawLine2005/Form1.cs
@@ -0,0 +1,261 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kalman2005
+{
+    public partial class KalmanFilter : Form
+    {
+        public KalmanFilter()
+        {
+            InitializeComponent();
+            InitializeParameter();
+            InitializeDraw();
+        }
+
+        private void InitializeParameter()
+        {
+            //pi = Math.PI;
+            mr = 0.9;
+            GetModulationRadio();
+
+            Y_LEN = pictureBox1.Height;
+            X_LEN = pictureBox1.Width;
+            if (X_LEN > 720)
+            {
+                X_LEN = 720;
+            }
+            else if (X_LEN < 360)
+            {
+                X_LEN = 360;
+            }
+
+            Ub = (double)Y_LEN;                 //Voltage base.
+            xtoa = (double)(CIRCLE) / X_LEN;    //x to angle.       x/X_LEN = i/360.   i = x * (360/X_LEN)
+        }
+
+        //=================================================================//background.
+        private void InitializeDraw()
+        {
+            bmp = new Bitmap(X_LEN, Y_LEN);
+            grp = Graphics.FromImage(bmp);
+            DrawPictureFrameLines(grp);
+            pictureBox1.Image = bmp;
+        }
+
+        private void DrawGrayFromStartToTargetPoints(int px1, int py1, int px2, int py2, int width)
+        {
+            grp.DrawLine(new Pen(Color.Gray, width), new Point(px1, py1), new Point(px2, py2));
+        }
+
+        private void DrawPictureFrameLines(Graphics grp)
+        {
+            //Top board line.
+            DrawGrayFromStartToTargetPoints(0, Y_LEN - Y_LEN, X_LEN, Y_LEN - Y_LEN, 2); //see the "-" right data (0, 0)->(0, Y).
+            //Bottom board line.
+            DrawGrayFromStartToTargetPoints(0, Y_LEN - 0, X_LEN, Y_LEN - 0, 2);
+            //Left board line.
+            DrawGrayFromStartToTargetPoints(0, Y_LEN - 0, 0, Y_LEN - Y_LEN, 2);
+            //Right board line.
+            DrawGrayFromStartToTargetPoints(X_LEN, Y_LEN - 0, X_LEN, Y_LEN - Y_LEN, 2);
+
+            //draw grad lines.
+            int columns = 12;                       //fix 30 degree grad. 360/30 = 12.
+            int gapa = CIRCLE / columns;            //angle gap.
+            float gapx = (float)(gapa / xtoa);      //x points gap.
+
+            int lines = 8;  // (int)(Y_LEN / grad);
+            float grad = (float)(Y_LEN / lines); //gapx;                      //y axis grad.
+
+            int flag = 0;
+            int dot = 5;
+            //draw dash lines.
+            for (int n = 0; n <= lines; n++ )
+            {
+                for (int p = 0; p < X_LEN; p = p + dot) //points
+                {
+                    if (flag == 0)
+                    {
+                        int cpy = (int)(n * grad); //lines point y axis.
+                        DrawGrayFromStartToTargetPoints(p, Y_LEN - cpy, p + dot, Y_LEN - cpy, 1); //from south-west.
+                        if (n == lines)
+                        {
+                            //Label xPole[];
+                        }
+                        flag = 1;
+                    }
+                    else
+                    {
+                        flag = 0;
+                    }
+                }
+            }
+            //draw dash columns.
+            for (int c = 0; c <= columns; c++)
+            {
+                for (int p = 0; p < Y_LEN; p = p + dot) //points
+                {
+                    if (flag == 0)
+                    {
+                        int cpx = (int)(c * gapa / xtoa); //column point x axis.
+                        DrawGrayFromStartToTargetPoints(cpx, Y_LEN - p, cpx, Y_LEN - (p + dot), 1); //from south-west.
+                        flag = 1;
+                    }
+                    else
+                    {
+                        flag = 0;
+                    }
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void DrawPictureDataLines(Graphics grp)
+        {
+            for (int x = 0; x < X_LEN; x++)
+            {
+                int k = x;
+                if (k > X_LEN - 2)
+                {
+                    k = X_LEN - 2;
+                }
+                grp.DrawLine(new Pen(Color.Red, 2), new Point(x, Y_LEN - (int)DC_U[k]), new Point(x + 1, Y_LEN - (int)DC_U[k + 1]));
+                grp.DrawLine(new Pen(Color.Blue, 2), new Point(x, Y_LEN - (int)DC_V[k]), new Point(x + 1, Y_LEN - (int)DC_V[k + 1]));
+                grp.DrawLine(new Pen(Color.Green, 2), new Point(x, Y_LEN - (int)DC_W[k]), new Point(x + 1, Y_LEN - (int)DC_W[k + 1]));
+                //grp.DrawLine(new Pen(Color.LightPink, 2), new Point(x, Y_LEN - (int)DC_COM[k]), new Point(x + 1, Y_LEN - (int)DC_COM[k + 1]));
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void ClearCalculatedData()
+        {
+            for (int i = 0; i < X_LEN; i++)
+            {
+                DC_U[i] = 0;
+                DC_V[i] = 0;
+                DC_W[i] = 0;
+                DC_COM[i] = 0;
+            }
+            InitializeDraw();
+        }
+
+        //=================================================================//
+
+        private void GetModulationRadio()
+        {
+            double modulation = double.Parse(textBox1.Text);
+            mr = (double)modulation;
+            if (mr > 1)
+            {
+                mr = 1;
+                textBox1.Text = "1.0";
+            }
+            else if (mr < 0)
+            {
+                mr = 0;
+                textBox1.Text = "0.0";
+            }
+        }
+
+        private void Init_k(double Q, double R)
+        {
+            kal.X = 22;    //测量的初始值
+            kal.P = 1.2;   //后验状态估计值误差的方差的初始值（不要为0问题不大）
+            kal.A = 1;    //转换系数
+            kal.H = 1;    //观测系数
+            kal.Q = Q;    //预测（过程）噪声方差 影响收敛速率，可以根据实际需求给出
+            kal.R = R;    //测量（观测）噪声方差 可以通过实验手段获得
+        }
+
+        private void KalmanFilterUpdate(double zk)
+        {
+            double Xe = 0;
+            double Pe = 0;
+            Xe = (kal.A) * (kal.X);
+            Pe = (kal.A) * (kal.A) * (kal.P) + (kal.Q);
+            kal.K = (Pe * (kal.H)) / ((Pe) * (kal.H) * (kal.H) + (kal.R));
+            kal.X = Xe + (kal.K) * (zk - ((kal.H) * Xe));
+            kal.P = (1 - (kal.K) * (kal.H)) * Pe;
+        }
+
+        private void TestKalmanFilter(double[] kmX, double[] kmP, double[] kmK) //kalman data array parameters.
+        {
+            Init_k(/*double Q*/ 3, /*double R*/ 4);
+
+            double zk = Y_LEN/2;
+            int range = X_LEN;
+            for (int x = 0; x < range; x++)
+            {
+                //if (x < (range / 2))
+                {
+                    kal.Q = 2*(rQ.NextDouble());
+                    kal.R = 2*(rR.NextDouble());
+                }
+                if (x % 2 == 0)
+                {
+                    zk = Y_LEN / 2 + 5 * (kal.R);
+                }
+                else
+                {
+                    zk = Y_LEN / 2 - 5 * (kal.Q);
+                }
+                if (x % 50 == 0)
+                {
+                    zk = Y_LEN / 2 + 20 * (kal.R);
+                }
+                else
+                {
+                    zk = Y_LEN / 2 - 20 * (kal.Q);
+                }
+                if (x == 120)
+                {
+                    zk = zk + 20;
+                }
+                if (x == 150)
+                {
+                    zk = zk - 25;
+                }
+                if (x == 180)
+                {
+                    zk = zk - 10;
+                }
+                kal.R = kal.R * kal.R;
+                kal.Q = kal.Q * kal.Q;
+
+                if (x % 5 == 0)
+                {
+                    KalmanFilterUpdate(/*double zk*/ zk);
+                }
+                
+                kmX[x] = (kal.X);
+                kmP[x] = (kal.P)*10;
+                //kmK[x] = (kal.K)*100;
+                kmK[x] = (zk - 50);
+            }
+        }
+
+        //====================================================================//events.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearCalculatedData();
+            TestKalmanFilter(DC_U, DC_V, DC_W);
+            DrawPictureDataLines(grp);
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            ClearCalculatedData();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            GetModulationRadio();
+        }
+
+    }
+}
