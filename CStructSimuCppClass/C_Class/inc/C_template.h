48a5bf5c7f4065e420d3b6d7f2aff75a1a23c0ff
jerhua <jerry.hua@assaabloy.com>
2018-12-28 20:44:36 +0800

    finish.

--- CStructSimuCppClass/C_Class/inc/C_template.h
+++ CStructSimuCppClass/C_Class/inc/C_template.h
@@ -29,7 +29,7 @@
 #define DT_TYPE                     int                     //Default Template type.
 
 //02// single INHERIT macro.
-#define __INHERIT(BaseClass, SelfClass, ...)                \
+#define _L1_INHERIT(BaseClass, SelfClass, ...)              \
     union                                                   \
     {                                                       \
         BaseClass father##BaseClass;                        \
@@ -41,20 +41,48 @@
 
 //03// Father class Member macro.
 //! The name after "MemberOf_" must be the name of basic class.
-//! Cause the macro will be expended in "__INHERIT".
+//! Cause the macro will be expended in "_L1_INHERIT".
 #define MemberOf_CFather(SelfClass, TypeName)               \
     TypeName data;                                          \
     int (*Calc)(SelfClass* self, int p);
 
+//! multi-level inherit, define pre-level class member.
 #define MemberOf_CSun001(SelfClass, TypeName)               \
         MemberOf_CFather(SelfClass, TypeName)               \
         int val;
+
+// double inherit.
+#define MemberOf_CMother(SelfClass, TypeName)               \
+    TypeName data;                                          \
+    int (*Calc)(SelfClass* self, int p);
+
+#define _DD_INHERIT(BaseClass1, BaseClass2, SelfClass, ...) \
+    union                                                   \
+    {                                                       \
+        BaseClass1 father##BaseClass1;                      \
+        struct                                              \
+        {                                                   \
+            MemberOf_##BaseClass1(SelfClass, __VA_ARGS__)   \
+        }b1;                                                \
+    };                                                      \
+    union                                                   \
+    {                                                       \
+        BaseClass2 mother##BaseClass2;                      \
+        struct                                              \
+        {                                                   \
+            MemberOf_##BaseClass2(SelfClass, __VA_ARGS__)   \
+        }b2;                                                \
+    };
 
 //04// declare class.
 typedef C_class Father      CFather;
 typedef C_class Sun001      CSun001;
 typedef C_class Sun002      CSun002;
 typedef C_class Baby01      CBaby01;
+
+//for double inherit.
+typedef C_class Mother      CMother;
+typedef C_class Dear01      CDear01;
 
 C_class Father
 {
@@ -64,21 +92,33 @@ C_class Father
 
 C_class Sun001
 {
-    __INHERIT(CFather, CSun001, DT_TYPE);
+    _L1_INHERIT(CFather, CSun001, DT_TYPE);
     int val;
 };
 
 C_class Sun002
 {
-    __INHERIT(CFather, CSun002, DT_TYPE);
+    _L1_INHERIT(CFather, CSun002, DT_TYPE);
     int val;
 };
 
+//for two level inherit.
 C_class Baby01
 {
-    __INHERIT(CSun001, CBaby01, DT_TYPE);
+    _L1_INHERIT(CSun001, CBaby01, DT_TYPE);
     int cry;
 };
 
+// for double inherit
+C_class Mother
+{
+    MemberOf_CMother(CMother, DT_TYPE);
+    int val;
+};
+C_class Dear01
+{
+    _DD_INHERIT(CFather, CMother, CDear01, DT_TYPE);
+    int val;
+};
 
 #endif //_C_TEMPLATE_H_.
--- CStructSimuCppClass/C_Class/readme.md
+++ CStructSimuCppClass/C_Class/readme.md
@@ -1,3 +1,27 @@
-# eclipse create/import project, build project, then run to check result.
+------
+# Use eclipse SDK create/import project, build project, then run to check result.
+    Mainly purpose is simulate c++ class with c struct, refer to C_template.h for detail.
+    The bad thing is hard to find define errors.
+
+## 1. define FATHER macro
+
+## 2. define __INHERIT macro
+
+## 3. define member macro
+
+## 4. declare class.
+
+## 5. define double base classes inherit.
+
+------
+# Test code:
+
+## 1. test base inherit.
+
+## 2. test polymorphism with sun001, sun002.
+
+## 3. test two level inherit with baby01.
+
+## 4. test double base classes inherit with mother and dear01.
 
 - email: huamg12@126.com
